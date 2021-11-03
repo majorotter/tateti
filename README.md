e05a814d04d48818f3f3051c6b069d04b2d4e5dc
majorotter <mariajoserotter@fi.uncoma.edu.ar>
2021-11-02 21:50:36 -0300

    tateti iniciado

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tateti
+
 Proyecto php para jugar al tateti en Introduccion a la Programación (FAI)
+
+## Materia 2021
+
+Introducción a la Programación
+Tecnicaturas
+FAI
+Universidad del Comahue
+
+## Integrantes del equipo
+
+- **Juan Perez** - legajo 1111 - mail:juacito@gmail.com - usuario github: juanperez
+- **Morena Garcia** - legajo 2222 - mail:morenita@fi.uncoma.edu.ar - usuario github:
---
+++ programaPerezGarcia.php
@@ -0,0 +1,25 @@
+<?php
+include_once("tateti.php");
+
+/**************************************/
+/***** DATOS DE LOS INTEGRANTES *******/
+/**************************************/
+
+/* Apellido, Nombre. Legajo. Carrera */
+/* ... COMMPLETAR ... */
+
+
+/**************************************/
+/***** DEFINICION DE FUNCIONES ********/
+/**************************************/
+
+
+
+
+
+/**************************************/
+/*********** PROGRAMA PRINCIPAL *******/
+/**************************************/
+$juego = jugar();
+//print_r($juego);
+//imprimirResultado($juego);
